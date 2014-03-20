59fae73649ca3829643e4dd5b99a02387beb30fb
James Yang <jamesyang124@gmail.com>
2014-03-20 02:49:19 -0700

    extract_min done, modify self.head to @head for attribute access level concern

--- binomial_heap.rb
+++ binomial_heap.rb
@@ -18,6 +18,14 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+
+# Binomial tree properties:
+# 1. N nodes binomial heap at most lg(n) + 1 binomial tree
+# 2. each key is the larger or greater than its parent => each root has smallest key in that tree
+#     => min-heap-ordered
+# 3. For any non-negative integer K, there has at most 1 binomial tree's root degree is K.
+# 4. Each tree's node count must be 2^i, i >= 0, i is that tree's degree
+
 class BinomialNode 
   # :next as sibling tree, :key as value
   attr_accessor :degree, :sibling, :child, :parent, :key
@@ -27,12 +35,24 @@ class BinomialNode
     self.key = key
     self.child = nil && self.sibling = nil && self.parent = nil
   end
+
+  def < (node)
+    self.key < node.key
+  end
 end
 
 class BinomialHeap 
-  attr_accessor :size, :min, :head
+  attr_reader :min, :head
+
+  # self.head === @head for instance object.
+  # "self.head=" expression call object's "head=" method so must declare :head to attr_writer
+  # if we have "self.head = something" expression.
   def initialize key
-    @head = BinomialNode.new key
+    if key.class != BinomialNode
+      @head = BinomialNode.new key 
+    else
+      @head = key
+    end
   end
 
   # create new heap for insert key, then merge to original heap
@@ -40,11 +60,78 @@ class BinomialHeap
     head = merge_root_list(self, BinomialHeap.new(key))
     h_next = head.sibling
     merge(head, h_next)
-    #print_siblings
   end
 
-  def delete
+  def min
+    get_min
+  end
+
+  # N nodes binomial heap at most lg(n) + 1 binomial tree
+  # 13 = 1101(binary representation) => 3 trees.
+  # at most lg n + 1 => O(lg(n))
+  def get_min
+    if next_head = self.head.sibling
+      current_head = self.head
+      while next_head 
+        next_head < current_head ? @min = next_head : @min = current_head
+        current_head = next_head
+        next_head = next_head.sibling  
+      end
+    else
+      @min = self.head
+    end
+    @min
+  end
+
+  # decrease a key than delete a node.
+  def delete_key
     
+  end
+
+  # delete min node in root of binomial tree.
+  def extract_min
+    extract_key = get_min.key
+
+    # create a new heap's root list from the min tree.
+    # Also reverse the order of min-tree's children to match the strictly increased order.
+    child = @min.child
+    root_list = []
+    while child
+      root_list << child
+      child = child.sibling
+    end
+    min_head = root_list.reverse!.shift
+    current = min_head
+    
+    until root_list.empty?
+      current.sibling = root_list.first
+      current = root_list.shift 
+    end
+    current.sibling = nil
+
+    # remove @min from current heap
+    root = self.head 
+    if root == @min
+      self.head = @min.sibling
+    else
+      prev = nil 
+      while root
+        if root == @min
+          prev.sibling = @min.sibling if prev
+          break
+        end
+        prev = root
+        root = root.sibling
+      end
+    end
+
+    # Union
+    head = merge_root_list(self, BinomialHeap.new(min_head))
+    h_next = head.sibling
+    merge(head, h_next)
+    # get new @min
+
+    extract_key
   end
 
   def print_siblings
@@ -80,7 +167,7 @@ private
       @head = hy_next
       hy_next = hy_next.sibling
     end
-    tail = @head
+    tail = self.head
 
     # recreate new root list for two heaps
     while hx_next && hy_next
@@ -100,19 +187,21 @@ private
       tail.sibling = hy_next
     end
 
-    return @head
+    return self.head
   end
 
   def merge current_head, next_head
     previous_head = nil
     while next_head 
       # Root list has strictly increse order by degree.
+      # In case current_head.degree = next_head.degree = next_head.sibling.degree, 
+      # we merge next_head and next_head.sibling in later, and left current_head unchanged. 
       if current_head.degree != next_head.degree \
         || (next_head.sibling && next_head.sibling.degree == current_head.degree) 
         previous_head = current_head
         current_head = next_head
       else
-        # compare two head's key value, min will be new head.
+        # compare two head's key value, min will be new root.
         if current_head.key > next_head.key
           # if previous head already exist, then the head of root list will not change any more.
           previous_head ? (previous_head.sibling = next_head) : (@head = next_head)
--- spec/binomial_heap_spec.rb
+++ spec/binomial_heap_spec.rb
@@ -1,16 +1,22 @@
 require "spec_helper"
 
 describe "BinomialHeap" do
-  it "insert key 0" do
-    x = BinomialHeap.new 18
-    x.insert 0
-    x.insert 100
-    x.insert 20
-    x.insert 5
-    x.insert -8
-    x.insert -9
-    x.insert -10
-    #expect(x.head.key).to equal 5
+  let(:x) { BinomialHeap.new 18 }
+  before :each do
+    @x = x 
+    @x.insert 0
+    @x.insert 100
+    @x.insert 20
+    @x.insert 5
   end
 
+  it "insert key 18, 0, 100, 20, 5" do
+    expect(@x.get_min.key).to equal 0
+    expect(@x.head.key).to equal 5
+  end
+
+  it "extract min" do
+    expect(@x.min.key).to equal 0
+    expect(@x.extract_min).to equal 0
+  end
 end 
