291edfb54cc9fca2e047397fa2346706571f7f76
James Yang <jamesyang124@gmail.com>
2014-04-07 16:18:46 -0700

    finish deletion, rotation, rspec testing for avl_tree

--- avl_tree.rb
+++ avl_tree.rb
@@ -23,14 +23,14 @@ class AvlTree
     current = root
     while current
       if current.key > key
-        current = current.left
+        current = current.left_node
       elsif current.key < key
-        current = current.right
+        current = current.right_node
       else
         break
       end
     end
-    puts "#{current ? "Not found" : "Found"} key #{key}."
+    puts "#{current ? "Found" : "Not found"} key #{key}."
     current
   end
 
@@ -48,12 +48,85 @@ class AvlTree
     end
     direction == 1 ? current.right_node = node : current.left_node = node
     node.parent = current
-    balance node 
-    morris_traversal   
+    balance node
   end
 
   def avl_delete(key)
-    
+    result = search_key key
+    if result
+      # no child or one child
+      if !result.left_node || !result.right_node
+        tmp = result.right_node ? result.right_node : result.left_node
+        if result.parent
+          tmp.parent = result.parent if tmp
+          if result.parent.left_node == result 
+            result.parent.left_node = tmp 
+          else
+            result.parent.right_node = tmp
+          end
+          get_height result.parent
+          get_balance_factor result.parent
+          balance result.parent
+        else
+          @root = tmp
+          tmp.parent = nil if tmp
+        end
+      else
+        # result has 2 children
+        # only 1 child for result's children, because AVL tree heights differ only at most 1, so suleaf's child has no children.
+        
+        successor = result.right_node
+        # find inorder successor, and replace, balance.
+        # successor at most 1 child. onlt in right, otherwise succesor will not be left-most node. 
+        while successor.left_node
+          successor = successor.left_node
+        end
+        suc_parent = successor.parent
+        
+        if parent = result.parent
+          if parent.left_node == result
+            parent.left_node = successor 
+          else
+            parent.right_node = successor 
+          end
+        else
+          @root = successor if parent == nil  
+        end
+        
+        successor.left_node = result.left_node
+        successor.left_node.parent = successor 
+        
+        if suc_parent != result
+          suc_parent.left_node = successor.right_node
+          successor.right_node = result.right_node
+        end        
+
+        successor.right_node.parent = successor if successor.right_node
+        successor.parent = parent  
+        
+        # find successor's parent's deepest then update it.
+        deepest_leaf = suc_parent
+        while deepest_leaf
+          if deepest_leaf.left_node && deepest_leaf.right_node
+            if deepest_leaf.left_node.height > deepest_leaf.right_node.height
+              deepest_leaf = deepest_leaf.left_node
+            else
+              deepest_leaf = deepest_leaf.right_node
+            end
+          else
+            if deepest_leaf.left_node
+              deepest_leaf = deepest_leaf.left_node
+            else
+              deepest_leaf.right_node ? deepest_leaf = deepest_leaf.right_node : break
+            end
+          end
+        end
+
+        #require 'pry'; binding.pry
+        balance deepest_leaf
+      end
+      result = nil
+    end
   end
 
   # direction determine new node in left or right child.
@@ -99,19 +172,20 @@ class AvlTree
         if pre.right_node
           pre.right_node = nil
           visit current
-          puts "current node: #{current.key}, height: #{current.height}, balance_factor: #{current.balance_factor}"
+          puts "current node #{current.key}, bf: #{current.balance_factor}, height: #{current.height}, parent: #{current.parent.key if current.parent} "
           current = current.right_node
         else
           pre.right_node = current
           current = current.left_node
         end
       else
-          puts "current node: #{current.key}, height: #{current.height}, balance_factor: #{current.balance_factor}"
         visit current
+          puts "current node #{current.key}, bf: #{current.balance_factor}, height: #{current.height}, parent: #{current.parent.key if current.parent} "
         current = current.right_node
       end
     end
     print_traversal_list
+    return nil
   end
 
   def print_traversal_list
@@ -120,7 +194,7 @@ class AvlTree
       traverse_list.each do |e|
         ary << e.key 
       end
-      puts ary.inspect
+      puts "Traversing order: #{ary.inspect}"
     else
       puts "No traverse made."
     end
@@ -209,6 +283,8 @@ private
   end
 
   def RLrotate(parent, child)
+    #require 'pry'; binding.pry
+    #puts "RL"
     sub_left = child.left_node
 
     parent.right_node = sub_left.left_node
@@ -219,7 +295,7 @@ private
     sub_left.parent = parent.parent
     child.parent = sub_left
     if parent.parent
-      parent.parent && parent.parent.left_node == parent ? parent.parent.left_node = sub_left : parent.parent.right_node = sub_left
+      parent.parent.left_node == parent ? parent.parent.left_node = sub_left : parent.parent.right_node = sub_left
     else
       @root = sub_left if parent == root
     end
@@ -233,9 +309,6 @@ private
     get_balance_factor parent
     get_balance_factor child
     update_balance_factor parent
-
-    #puts "RL"
-    #require 'pry'; binding.pry
   end
 
   def get_height(node)
@@ -266,14 +339,14 @@ private
   end
 
   def update_balance_factor(node)
-    while node.parent
+    while node && node.parent
       get_balance_factor node.parent
       node = node.parent
     end
   end
 
   def update_height(node)
-    while node.parent
+    while node && node.parent
       get_height node.parent
       node = node.parent
     end
--- spec/avl_tree_spec.rb
+++ spec/avl_tree_spec.rb
@@ -14,6 +14,7 @@ describe "AvlTree" do
   it "#print_traversal_list" do
     a = AvlTree.new 8
     a.send(:visit, a.root)
+    puts ""
     expect(a.send(:print_traversal_list)).to include(a.root.key)
   end
 
@@ -27,6 +28,49 @@ describe "AvlTree" do
     a.avl_insert 11
     a.avl_insert 12
     a.avl_insert 8.5
+    puts ""
+    a.morris_traversal
+    expect(a.root.key).to equal 9
+  end
+
+  it "#avl_search" do
+    a = AvlTree.new 8
+    a.avl_insert 1
+    a.avl_insert 5
+    a.avl_insert 9
+    a.avl_insert 10
+    a.avl_insert 511
+    a.avl_insert 11
+    a.avl_insert 12
+    a.avl_insert 8.5
+    puts ""
+    result = a.search_key 8
+    expect(result.key).to equal 8
+    puts ""
+    result = a.search_key 100
+    expect(result).to be_nil
+  end
+
+  it "#avl_delete" do
+    a = AvlTree.new 8
+    a.avl_insert 1
+    a.avl_insert 5
+    a.avl_insert 9
+    a.avl_insert 10
+    a.avl_insert 511
+    a.avl_insert 11
+    a.avl_insert 12
+    a.avl_insert 8.5
+    puts ""
+    a.avl_delete 8
+    a.avl_delete 1
+    a.avl_delete 5
+    a.avl_delete 9
+    a.avl_delete 10
+    a.avl_delete 511
+    a.avl_delete 11
+    a.avl_delete 12
+    a.avl_delete 8.5
     a.morris_traversal
   end
 end
