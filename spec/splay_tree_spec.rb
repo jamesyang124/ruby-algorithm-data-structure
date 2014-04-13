f5a9b6e160a4ba69a334c759f926c1cf6dff3e59
James Yang <jamesyang124@gmail.com>
2014-04-13 15:05:10 -0700

    rspec for splay_tree

--- spec/splay_tree_spec.rb
+++ spec/splay_tree_spec.rb
@@ -3,30 +3,67 @@ require_relative "../splay_tree.rb"
 
 describe "SplayTree" do 
   it "create a new splay tree" do
-    
+    a = SplayTree.new 8
+    expect(a.root.key).to equal 8
   end
 
   it "insert new elements" do
-    
+    a = SplayTree.new 8
+    a.insert 1
+    a.insert 5
+    a.insert 9
+    a.insert 10
+    a.insert 511
+    a.insert 11
+    a.insert 12
+    a.insert 8.5
+    expect(a.root.key).to equal 8.5
   end
 
   it "search the key" do
-    
-  end
-
-  it "delete a key" do
-    
+    a = SplayTree.new 8
+    a.insert 1
+    a.insert 5
+    a.insert 9
+    a.insert 10
+    a.insert 511
+    a.insert 11
+    a.insert 12
+    a.insert 8.5
+    puts ""
+    result = a.search 8.5
+    expect(result.key).to equal 8.5
   end
 
   it "get subtree minimum" do
-    
+    a = SplayTree.new 8
+    a.insert 1
+    a.insert 5
+    a.insert 9
+    a.insert 10
+    a.insert 511
+    a.insert 11
+    a.insert 12
+    a.insert 8.5
+    result = a.send(:subtree_minimum, a.root)
+    expect(result.key).to equal 1
   end
 
   it "get subtree maximum" do
-    
+    a = SplayTree.new 8
+    a.insert 1
+    a.insert 5
+    a.insert 9
+    a.insert 10
+    a.insert 511
+    a.insert 11
+    a.insert 12
+    a.insert 8.5
+    result = a.send(:subtree_maximum, a.root)
+    expect(result.key).to equal 511
   end
 
-  it "test private #splay" do
+  it "delete a key" do
     
   end
 end
--- splay_tree.rb
+++ splay_tree.rb
@@ -16,11 +16,12 @@ class SplayTree
     current = root
     while current
       node = current
-      current = current.key > key ? current.r : current.l
+      current = current.key < key ? current.r : current.l
     end
     current = SplayNode.new key
-    node.key > key ? node.r = current : node.l = current
+    node.key < key ? node.r = current : node.l = current
     current.parent = node
+    splay(current)
   end
 
   def search(key)
@@ -40,19 +41,22 @@ class SplayTree
 
   def delete(key)
     result = search key
-    if resulst
-      # no or 1 child only
-      if !result.l || !result.r
-        child = result.l || result.r
-        if result.parent
-          result.parent.l == result ? (result.parent.l = child) : (result.parent.r = child)
-          child.parent = result.parent
-        else
-          child.parent = nil
-          @root = child
-        end
+    if result
+      splay(result)
+      if !result.l
+        replace(result, result.r) 
+      elsif !result.r
+        replace(result, result.l)
       else
-        # two children
+        sub_min = subtree_minimum result.r
+        if sub_min.parent != result
+          replace(sub_min, sub_min.r)
+          sub_min.r = result.r
+          sub_min.r.parent = sub_min
+        end
+        replace(result, sub_min)
+        sub_min.l = result.l
+        sub_min.l.parent = result
       end
     end
   end
@@ -92,11 +96,10 @@ private
     node.r = r_child.l
     r_child.l.parent = node if r_child.l
     r_child.parent = node.parent
-    @root = r_child if !node.parent
-    if node == node.parent.l
-      node.parent.l = r_child
+    if node.parent 
+      node == node.parent.l ? node.parent.l = r_child : node.parent.r = r_child
     else
-      node.parent.r = r_child
+      @root = r_child
     end
     r_child.l = node
     node.parent = r_child
@@ -113,46 +116,54 @@ private
     node.l = l_child.r
     l_child.r.parent = node if l_child.r    
     l_child.parent = node.parent
-    @root = l_child if !node.parent
-    if node == node.parent.l
-      node.parent.l = l_child
+    if node.parent
+      node == node.parent.l ? node.parent.l = l_child : node.parent.r = l_child
     else
-      node.parent.r = l_child
+      @root = l_child 
     end
     l_child.r = node
     node.parent = l_child
   end
 
-  def replace
-    
+  def replace(node_u, node_v)
+    if !node_u.parent
+      @root = node_v 
+    elsif node_u == node_u.parent.left
+      node_u.parent.left = node_v
+    else
+      node_u.parent.right = node_v
+    end  
+    node_v.parent = node_u.parent if node_v
   end
 
   def splay(node)
     while node.parent
       if (!node.parent.parent)
-        node.parent.left == node ? right_rotate(node.parent) : left_rotate(node.parent)
-      elsif node.parent.left == node && node.parent.parent.left == node.parent
+        node.parent.l == node ? right_rotate(node.parent) : left_rotate(node.parent)
+      elsif node.parent.l == node && node.parent.parent.l == node.parent
         right_rotate(node.parent.parent)
         right_rotate(node.parent)
-      elsif node.parent.right == node && node.parent.parent.right == node.parent
+      elsif node.parent.r == node && node.parent.parent.r == node.parent
         left_rotate(node.parent.parent)
         left_rotate(node.parent)
-      elsif node.parent.left == node && node.parent.parent.right == node.parent
+      elsif node.parent.l == node && node.parent.parent.r == node.parent
         right_rotate(node.parent)
-        left_rotate(node.parent.parent)
+        left_rotate(node.parent)
       else
         left_rotate(node.parent)
-        right_rotate(node.parent.parent)
+        right_rotate(node.parent)
       end  
     end
   end
 
   def subtree_minimum(node)
-    
+    node = node.l while node.l
+    node
   end
 
   def subtree_maximum(node)
-    
+    node = node.r while node.r
+    node
   end
 
   def visit(node)
