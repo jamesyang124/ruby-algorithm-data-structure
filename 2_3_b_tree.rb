940bc2e1a260e052c972fda6457ef738fbbe7c54
James Yang <jamesyang124@gmail.com>
2014-04-23 17:50:48 -0700

    deletion done, start rpsec test for private #internal_inorder_pred test

--- 2_3_b_tree.rb
+++ 2_3_b_tree.rb
@@ -35,7 +35,33 @@ class Btree
   end
 
   def delete(key)
-    
+    # rotate
+    # substitution nearest value to that position
+    result = find_leaf key
+
+    if result.nary.empty? # result is leaf
+      if result.size == 2
+        result.key_l == key ? result.key_l = nil : result.key_r = nil
+        result.size -= 1 
+      else
+        result.key_l = nil
+        result.size = 0
+        handle_hole result
+      end
+    else  # result is internel node
+      pred = inorder_predecessor result, key
+      if pred.size == 2
+        result.key_l == key ? result.key_l = pred.key_r : result.key_r = pred.key_r
+        pred.key_r = nil
+        pred.size = 1
+      else
+        result.key_l == key ? result.key_l = pred.key_l : result.key_r = pred.key_l
+        pred.key_l = nil
+        pred.size = 0
+        hole = pred
+        handle_hole hole
+      end
+    end
   end
 
   def print_list
@@ -43,7 +69,7 @@ class Btree
     ary = []
     ary << current
     while current
-      printf "Current: %-7s, node_left: %-2s, node_middle: %-2s, node_right: %-2s, parent: %5s\n", "#{current.key_l} / #{current.key_r}", "#{current.nary[0].key_l if current.nary[0]}", "#{current.nary[1].key_l if current.nary[1]}","#{current.nary[2].key_l if current.nary[2]}", "#{current.p.key_l if current.p} / #{current.p.key_r if current.p}"
+      printf "Current: %-7s, node_left: %-2s, node_middle: %-2s, node_right: %-2s, parent: %5s, nsize: #{current.nary.size}\n", "#{current.key_l} / #{current.key_r}", "#{current.nary[0].key_l if current.nary[0]}", "#{current.nary[1].key_l if current.nary[1]}","#{current.nary[2].key_l if current.nary[2]}", "#{current.p.key_l if current.p} / #{current.p.key_r if current.p}"
       ary << current.nary[0] if current.nary[0]
       ary << current.nary[1] if current.nary[1]
       ary << current.nary[2] if current.nary[2]
@@ -53,6 +79,131 @@ class Btree
   end
 
 private
+
+  # find either prde. or succe., both are fine to replace with.
+  def internal_inorder_pred(node, key)
+    # internal node
+    leaf = (key == node.key_l) ? node.nary[0] : node.nary[1]
+    while leaf
+      pred = leaf
+      leaf = leaf.nary[2] ? leaf.nary[2] : leaf.nary[1]
+    end
+    pred
+  end
+
+  # hole is special node has only one link because its a combination from sub-leaf node to current node.
+  def handle_hole(hole)
+    if hole.parent.size == 1
+      loc, sib = get_2_node_sib hole.parent, hole
+      if loc == 1
+        if sib.size == 1
+          # hole has 2-node parent, 2-node right sibling, rotation
+          sib.nary.insert(0, hole.nary[0])
+          sib.key_l, sib.key_r = hole.parent.key_l, sib.key_l
+          hole.parent.key_l = nil
+          hole.parent.nary.clear << sib
+          sib.size = 2
+          hole.parent.size = 0
+          handle_hole(hole.parent)
+        else
+          # hole has 2-node parent, 3-node right sibling, rotation
+          hole.key_l, sib.parent.key_l, sib.key_l , sib.key_r = sib.parent.key_l, sib.key_l , sib.key_r, nil
+          hole.nary << sib.nary.shift 
+          hole.size = 1
+          sib.size = 1
+        end
+      else
+        if sib.size == 1
+          # hole has 2-node parent, 2-node left sibling, rotation
+          sib.nary << hole.nary[0]
+          sib.key_r = hole.parent.key_l
+          hole.parent.key_l = nil
+          hole.parent.nary.clear << sib
+          hole.parent.size = 0
+          sib.size = 2
+          handle_hole(hole.parent)
+        else
+          # hole has 2-node parent, 3-node left sibling, rotation
+          hole.key_l, sib.parent.key_l, sib.key_r = sib.parent.key_l, sib.key_r , sib.key_r, nil
+          hole.nary << sib.nary.pop
+          hole.size = 1
+          sib.size = 1
+        end
+      end
+    elsif hole.parent.size == 2
+      loc, sib = get_3_node_sib hole.parent, hole
+      if loc == 0
+        if sib.size == 1
+          # hole has 3-node parent, 2-node left sibling, rotation  
+          sib.key_r, hole.parent.key_r = hole.parent.key_r, nil
+          sib.nary.concat hole.nary
+          hole.parent.nary.pop
+          hole.parent.size = 1
+          sib.size = 2
+        else
+          # hole has 3-node parent, 3-node left sibling, rotation
+          hole.key_l, hole.parent.key_r, sib.key_r = hole.parent.key_r, sib.key_r, nil
+          hole.nary.insert(0, sib.nary.pop)
+          hole.size = 1
+          sib.size = 1  
+        end
+      elsif loc == 1
+        # hole has 3-node parent, 2-node right sibling, rotation
+        if sib.size == 1 
+          sib.nary.insert(0, hole.nary[0])
+          sib.key_l, sib.key_r, hole.parent.key_l, hole.parent.key_r = hole.parent.key_l, sib.key_l, hole.parent.key_r, nil
+          hole.parent.nary.shift 
+          hole.parent.size = 1
+          sib.size = 2
+        else
+          # hole has 3-node parent, 3-node right sibling, rotation  
+          hole.key_l, hole.parent.key_l, sib.key_l, sib.key_r = hole.parent.key_l, sib.key_l, sib.key_r, nil
+          hole.nary << sib.nary.shift
+          hole.size = 1
+          sib.size = 1
+        end
+      else
+        # hole in middle, prefer to pick left sib(right is also ok).
+        if sib.size == 1
+          # hole in middle and has 3-node parent, 2-node sibling.
+          sib.key_r, hole.parent.key_l, hole.parent.key_r = hole.parent.key_l, hole.parent.key_r, nil
+          sib.nary.concat hole.nary
+          hole.parent.nary.delete_if { |e| e == hole }
+          hole.parent.size = 1
+          sib.size = 2 
+        else
+          # hole in middle and has 3-node parent, 3-node sibling.
+          hole.key_l, hole.parent.key_l, sib.key_r = hole.parent.key_l, sib.key_r, nil
+          hole.nary.insert(0, sib.nary.pop)
+          hole.size = 1
+          sib.size = 1  
+        end
+      end
+    else
+      # hole in hole.parent. It should not happened.
+    end
+  end
+
+  def get_3_node_sib(parent, node)
+    if parent.nary[0] == node 
+      return 1, parent.nary[1] if parent.nary[1].size == 1
+    elsif parent.nary[2] == node
+      # In here we prefer take left side sib, take right side sib also fine.
+      return 0, parent.nary[1] if parent.nary[1].size == 1
+    else
+      return 2, parent.nary[0] if parent.nary[0].size == 1
+    end
+  end
+
+  def get_2_node_sib(parent, node)
+    if parent.nary[0] == node
+      # sib in right
+      return 1, parent.nary[1]
+    else
+      # sib in left
+      return 0, parent.nary[0]
+    end
+  end
 
   # aux is a children list for rearranging new children.
   def split(node, key, aux = nil)
--- spec/2_3_b_tree_spec.rb
+++ spec/2_3_b_tree_spec.rb
@@ -2,6 +2,27 @@ require "spec_helper"
 require_relative '../2_3_b_tree'
 
 describe "Btree" do
+  let(:create_tree) do 
+    a = Btree.new 8
+    a.insert 15
+    a.insert 9
+    a.insert 1
+    a.insert 7
+    a.insert 10
+    a.insert 13
+    a.insert 11
+    a.insert 12
+    a.insert 10.5
+    a.insert 12.5
+    a.insert 16
+    a.insert 17
+    a.insert 18
+    a.insert 19
+    a.insert 20
+    a.insert 21
+    a
+  end
+
   it "create a new b tree" do
     a = Btree.new 8 
     expect(a.root.key_l).to equal 8
@@ -27,5 +48,13 @@ describe "Btree" do
     a.insert 21
     puts ""
     a.print_list
+  end
+
+  it "private #inorder_predecessor" do
+    tree = create_tree
+    pred = tree.send(:internal_inorder_pred, tree.root, 13)
+    expect(pred.key_r).to equal 12.5
+    pred = tree.send(:internal_inorder_pred, pred.p, 11)
+    expect(pred.key_r).to equal 10.5
   end
 end
