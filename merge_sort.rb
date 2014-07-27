1e655e19c4fd5b1b6f47a0f4646444518567d999
James Yang <jamesyang124@gmail.com>
2014-07-27 13:56:58 -0700

    add merge sort rewrite

--- merge_sort.rb
+++ merge_sort.rb
@@ -93,3 +93,50 @@ end
 
 bottom_up_merge_sort(ary2, bkp2)
 
+
+
+ary2 = [37, 41, 19, 81, 41, 25, 56, 61, 49]
+# merge sort rewrite
+
+def re_copy(ary, bkp)
+  bkp.each.with_index { |e, i| ary[i] = e }
+end
+
+def re_merge(ary, left, mid, right, bkp)
+  l = left
+  m = mid
+  k = l
+  while k < right
+    if l < mid && ( m >= right || ary[l] < ary[m])
+      bkp[k] = ary[l]
+      l += 1
+    else
+      bkp[k] = ary[m]
+      m += 1
+    end
+    k += 1
+  end
+end
+
+def re_merge_sort(ary)
+  size = ary.size
+  width = 1
+  bkp = []
+
+  while width < size
+    # iteration for each block.
+    i = 0
+    while i < size
+      mid = (i + width) < size ? (i + width) : size
+      right = (i + 2 * width) < size ? (i + 2 * width) : size 
+      re_merge(ary, i, mid, right, bkp)
+      i = i + 2 * width
+    end
+    re_copy(ary, bkp)
+    width = width * 2 
+  end
+  ary
+end
+
+puts "Rewrite"
+p re_merge_sort(ary2)
